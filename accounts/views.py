fa44bf939a11c7cc7384ff9cf151c4510a24f2f5
assem <assem@thefoot.com>
2021-03-22 19:55:42 +0600

    hotfix

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,7 @@ from .models import User, PhoneOTP
 from rest_framework import generics
 
 
-class ValidatePhoneSendCode(generics.ListAPIView):
+class ValidatePhoneSendCode(generics.CreateAPIView):
     def post(self, request, *args, **kwargs):
         phone_number = request.data.get('phone')
         if phone_number:
